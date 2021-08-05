bd62af294b3f7f12c880c8b689627aebb7efe881
Andres-eiptpl <andresdeblas15@gmail.com>
2021-08-04 22:48:47 -0500

    Add Dashboard xd
    Agregamos dashboard page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
   {
     path: '',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
   }
+
 ];
 @NgModule({
   imports: [
