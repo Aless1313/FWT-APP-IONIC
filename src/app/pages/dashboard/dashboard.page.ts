60be44182440bbf8442283dc20e73994a9f9f991
Andres-eiptpl <andresdeblas15@gmail.com>
2021-08-05 01:14:50 -0500

    DashboardDiseño1
    Primera prueba de diseño de Dashboard

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class DashboardPage implements OnInit {
+export class DashboardPage implements OnInit, OnDestroy {
+  percentageValue: (value: number) => string;
 
-  constructor() { }
+  gaugeValues: any = {
+    1: 100,
+    2: 50,
+    3: 50,
+    4: 50,
+    5: 50,
+    6: 50,
+    7: 50,
+  };
+  interval: any;
+  constructor() {
+    this.percentageValue = function (value: number): string {
+      return `${Math.round(value)} / ${this['max']}`;
+    };
+   }
 
-  ngOnInit() {
+  ngOnInit(): void{
+    const updateValues = (): void => {
+      this.gaugeValues = {
+        1: Math.round(Math.random() * 20),
+        2: Math.round(Math.random() * 100),
+        3: Math.round(Math.random() * 100),
+        4: Math.round(Math.random() * 100),
+        5: Math.round(Math.random() * 200),
+        6: Math.round(Math.random() * 100),
+        7: Math.round(Math.random() * 100),
+      };
+  }
+  const INTERVAL: number = 1000;
+  
+  this.interval = setInterval(updateValues, INTERVAL);
+  updateValues();
   }
 
+ngOnDestroy(): void {
+  clearInterval(this.interval);
+}
 }
